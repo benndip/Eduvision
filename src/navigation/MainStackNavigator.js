31d84ed3b56cde5ad196824572d9eb1a8b199e52
Benndip <obentabiayuk1@gmail.com>
2022-06-04 04:05:41 +0100

    Resseting data

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Landing, Login, Signup, Onboarding } from '../screens'
-
-import BottomNavigation from './BottomNavigation';
+import { Landing, Login, Signup, Onboarding, Home, Details, ModelGroup } from '../screens'
 
 const Stack = createNativeStackNavigator();
 
 const MainStackNavigator = () => {
     return (
         <Stack.Navigator
-            initialRouteName="BottomNavigation"
+            initialRouteName="Landing"
             screenOptions={{
                 headerShown: false,
             }}
         >
-            <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
             <Stack.Screen name="Landing" component={Landing} />
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Signup" component={Signup} />
             <Stack.Screen name="Onboarding" component={Onboarding} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Details" component={Details} />
+            <Stack.Screen name="ModelGroup" component={ModelGroup} />
         </Stack.Navigator>
     )
 }
--- src/screens/index.js
+++ src/screens/index.js
@@ -2,7 +2,6 @@ import Home from '../screens/Home/Home.screen';
 import Landing from '../screens/Landing/Landing.screen';
 import Details from '../screens/Details/Details.screen';
 import ModelGroup from '../screens/ModelGroup/ModelGroup.screen';
-import ModelConceptsHome from '../screens/ModelConceptsHome/ModelConceptsHome.screen';
 import ConceptItem from '../components/ConceptItem/ConceptItem.component';
 import Signup from '../screens/Signup/Signup.screen';
 import Login from '../screens/Login/Login.screen';
@@ -13,7 +12,7 @@ export {
     Landing,
     Details,
     ModelGroup,
-    ModelConceptsHome,
+
     ConceptItem,
     Signup,
     Login,
